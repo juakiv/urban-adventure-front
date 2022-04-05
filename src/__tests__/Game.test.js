8a81abd7cf85b562da39a64c7e8f3fce45a98ca9
Jussi <juakiv@utu.fi>
2022-04-05 19:36:30 +0300

    Revert "test failing test in github"
    This reverts commit 45a759a2056d3428177324dbe80e2850b775535c.

--- src/__tests__/Game.test.js
+++ src/__tests__/Game.test.js
@@ -5,8 +5,7 @@ test('it can initiate game instance', () => {
   let context = canvas.getContext("2d");
   
   const game = new Game(canvas, context);
-  expect(game).toBe(5);
-  
+
   expect(game).toBeTruthy();
   expect(game).not.toBeNull();
   expect(game).toBeInstanceOf(Game);
