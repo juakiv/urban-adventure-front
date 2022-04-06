3dac00945130c3ad955268f14bc75f81966993f3
Jussi <juakiv@utu.fi>
2022-04-06 12:28:27 +0300

    add initial jumping mechanism and tests
    needs major refactoring at some point

--- src/game/Character/index.js
+++ src/game/Character/index.js
@@ -2,32 +2,70 @@ class Character {
   #canvas
   #context
 
-  #height
-
   #characterWidth
   #characterHeight
 
-  constructor(height) {
+  #isJumping
+  #gravity
+
+  #posY
+  #deltaY
+
+  constructor() {
     this.#canvas = document.createElement("canvas");
     this.#context = this.#canvas.getContext("2d");
 
-    this.#height = height;
     this.#characterWidth = 60;
     this.#characterHeight = 120;
 
     this.#canvas.width = this.#characterWidth;
     this.#canvas.height = this.#characterHeight;
 
+    this.#isJumping = false;
+    this.#gravity = 0.2;
+
+    this.#posY = 500 - this.#characterHeight;
+    this.#deltaY = 0;
+
     this.draw();
   }
 
   draw() {
-    this.#context.fillStyle = "#ff0000";
-    this.#context.fillRect(0, 0, this.#characterWidth, this.#characterHeight);
+    let image = new Image();
+    image.onload = () => {
+      this.#context.drawImage(image, 0, 0);
+    }
+    image.src = "/tikkuukko.png";
+    // this.#context.fillStyle = "#ff0000";
+    // this.#context.fillRect(0, 0, this.#characterWidth, this.#characterHeight);
+
+    return this.#canvas;
   }
 
-  getCharacter() {
-    return this.#canvas;
+  update() {
+    this.#deltaY += this.#gravity;
+    this.#posY += this.#deltaY;
+
+    if(this.#posY + this.#characterHeight >= 500) {
+      this.#isJumping = false;
+
+      this.#posY = 500 - this.#characterHeight;
+      this.#deltaY = 0;
+    }
+  }
+
+  jump() {
+    if(this.#isJumping) return; // ei kaksoishyppyjä ainakaan vielä
+    this.#isJumping = true;
+    this.#deltaY = -6;
+  }
+
+  getPosY() {
+    return this.#posY;
+  }
+
+  getIsJumping() {
+    return this.#isJumping;
   }
 
 }
--- src/game/index.js
+++ src/game/index.js
@@ -3,10 +3,19 @@ import Character from './Character';
 class Game {
   #canvas
   #context
+  #character
 
   constructor(canvas, context) {
     this.#canvas = canvas;
     this.#context = context;
+    this.#character = new Character();
+
+    // this.#character.jump();
+    window.addEventListener("keypress", e => {
+      // näppäinkomennot? tää varmaa kannattaa miettiä uusiks myöhemmässä
+      // mut nyt aluks ihan kelpo
+      if(e.code === "Space") this.#character.jump();
+    });
 
     this.render();
   }
@@ -16,8 +25,13 @@ class Game {
   }
 
   render() {
-    let character = new Character();
-    this.#context.drawImage(character.getCharacter(), 10, 10);
+    this.#context.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+    this.#context.fillStyle = '#000000';
+    this.#context.fillRect(0,0, this.#canvas.width, this.#canvas.height);
+
+    this.#character.update();
+    
+    this.#context.drawImage(this.#character.draw(), 20, this.#character.getPosY());
     requestAnimationFrame(() => this.render());
   }
 }
