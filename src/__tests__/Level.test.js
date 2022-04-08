eef8c478ad9885afd57d6bc7e7b9451f10c627ae
unknown <tukako@utu.fi>
2022-04-08 19:38:04 +0300

    muutama metodi satunnaisen platformin luontiin

--- src/__tests__/Level.test.js
+++ src/__tests__/Level.test.js
@@ -10,14 +10,56 @@ describe("Does Level instance exist correctly",() => {
     });
 
     test("Is there a Level", () => {
-        const lvl = new Level(canvas, context, 5);
+        const lvl = new Level(canvas, context, 5, 1);
         expect(lvl).toBeTruthy();
     });
 
     test("Has correct jump height after initialization",() =>{
-        const lvl = new Level(canvas, context, 5);
+        const lvl = new Level(canvas, context, 5, 1);
         expect(lvl.getJumpHeight()).toBe(5);
+    });
+
+    test("Has correct starting speed after initialization",() =>{
+        const lvl = new Level(canvas, context, 5, 1);
+        expect(lvl.getStartSpeed()).toBe(1);
     });
 
 
 });
+
+describe("Level functionality", () => {
+    let canvas
+    let context;
+    beforeEach(() => {
+        canvas = document.createElement("canvas");
+        context = canvas.getContext("2d");
+    });
+
+    test("Gives acceptable new height for the next platform", () => {
+        const lvl = new Level(canvas, context, 5, 1);
+        for(let i = 0; i < 1000; i++) {
+            const nextHeight = lvl.getNextPlatformHeight();
+        
+            expect(nextHeight).toBeLessThan(canvas.height);
+            expect(nextHeight).toBeGreaterThan(canvas.height/2 - lvl.getJumpHeight());
+        }
+    });
+
+    //currently I defined "okay" as a value starting from character width to 1/4 of canvas.width
+    test("Gives acceptable width for the next platform", () => {
+        const lvl = new Level(canvas, context, 5, 1);
+        const nextWidth = lvl.getNextWidth();
+        expect(nextWidth).toBeLessThan(canvas.width/4);
+        expect(nextWidth).toBeGreaterThanOrEqual(60); //current character width
+        
+    })
+
+    
+    //Position should be such that at least from one point of a current platform
+    // the next platform can be reached
+    test("Gives acceptable new x-position for the next platform", () => {
+        const lvl = new Level(canvas, context, 5, 1);
+        
+
+    });
+})
--- src/game/Level/index.js
+++ src/game/Level/index.js
@@ -3,16 +3,37 @@ class Level {
   #context;
 
   #jumpHeight;
+  #startSpeed;
 
-  constructor(canvas, context, jumpHeight) {
-      this.#canvas;
-      this.#context; 
+  constructor(canvas, context, jumpHeight, startSpeed) {
+      this.#canvas = canvas;
+      this.#context = context; 
       this.#jumpHeight = jumpHeight;
-  }
+      this.#startSpeed = startSpeed;
+      }
 
   getJumpHeight() {
       return this.#jumpHeight;
   }
+
+  getStartSpeed() {
+    return this.#startSpeed;
+  }
+
+  getNextPlatformHeight() {
+    const isHigher = Math.random();
+    if(isHigher > 0.5) {
+      // JumpHeightille vakio, jotta helpompi yltää perille
+      return this.#canvas.height/2 - Math.random()*(0.8*this.#jumpHeight);
+    }
+    // jaetaan 2.3, jottei alin palikka ole liian alhaalla
+    return this.#canvas.height/2 + Math.random()*this.#canvas.height/2.3;
+  }
+
+  getNextWidth() {
+    return 60 + Math.random() * (this.#canvas.width/4 - 60);
+  }
+
 }
 
 export default Level;
