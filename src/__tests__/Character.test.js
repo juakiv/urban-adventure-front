94e88efc89cd2d653b6334c10159fa32b4a59075
Jussi <juakiv@utu.fi>
2022-04-12 19:52:10 +0300

    fix tests

--- src/__tests__/Character.test.js
+++ src/__tests__/Character.test.js
@@ -2,16 +2,20 @@ import Character from "../game/Character";
 
 describe("character tests", () => {
   let character;
-
+  let canvas, context;
   beforeEach(() => {
-    character = new Character();
+    canvas = document.createElement("canvas");
+    context = canvas.getContext("2d");
+    character = new Character(canvas, context);
   })
 
   test("it can jump", () => {
     expect(character.getIsJumping()).toBeFalsy();
 
-    character.jump();
-    
+    character.setIsJumping(true);
     expect(character.getIsJumping()).toBeTruthy();
+    character.update();
+
+    expect(character.getPosY()).toBeLessThan(400);
   });
 });
