818d0113d857bb69402b36bfe5f939104e8b4f73
Jussi <juakiv@utu.fi>
2022-04-16 12:39:12 +0300

    muutettu websocket yhteyden muodostamista

--- src/MainGame.js
+++ src/MainGame.js
@@ -3,6 +3,10 @@ import Game from './game';
 
 const MainGame = props => {
     const canvasRef = useRef(null);
+
+    const [socket, setSocket] = useState(null);
+    const [connecting, setConnecting] = useState(true);
+
     const [game, setGame] = useState(null);
     const [score, setScore] = useState(null);
     const [username, setUsername] = useState("");
@@ -14,7 +18,14 @@ const MainGame = props => {
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        
+
+        const ws = new WebSocket(`ws://${process.env.NODE_ENV === 'production' ? "urban-adventure-game.herokuapp.com" : "localhost:3001"}`);
+        setSocket(ws);
+
+        ws.addEventListener("open", e => {
+            setConnecting(false);
+        });
+
         if(context != null) {
             context.fillStyle = '#000000';
             context.fillRect(0,0, context.canvas.width,
@@ -35,21 +46,13 @@ const MainGame = props => {
 
     function submitHandling() {
         if(!username.match(/\w+/)) {
-            alert("kelvoton nimi");
+            alert("Kelvoton nimi");
             return;
         }
-        
-        const socket = new WebSocket(`ws://${process.env.NODE_ENV === 'production' ? "urban-adventure-game.herokuapp.com" : "localhost:3001"}`);
-        socket.onopen = function(e) {
-            let userdata = JSON.stringify({ 
-                            "username" : username,
-                             "score" : score
-                            });
-            socket.send(userdata);
-            socket.close();
-            setIsNameValid(true);
-        } 
-        
+
+        let userdata = JSON.stringify({"username" : username, "score" : score });
+        socket.send(userdata);
+        setIsNameValid(true); 
     }
 
 
@@ -67,30 +70,29 @@ const MainGame = props => {
         <div id="main-game">
             {!isGameRunning ?
             <div id="ui">
-                {currentMenu === "main" && <div id="main-menu">
+                {connecting && <div id="main-menu">
+                    <h2>Connecting to the server...</h2>
+                </div>}
+                {!connecting && currentMenu === "main" && <div id="main-menu">
                     <img src="/logo.png" alt="" />
                     <button onClick={() => startGame()}>Start Game</button>
                     <button onClick={() => alert("Kesken...")}>High Scores</button>
                 </div>}
-                {currentMenu === "death" && <div id="death-menu">
+                {!connecting && currentMenu === "death" && <div id="death-menu">
                     <img src="/oops.png" alt="" />
                     <h2>Score: {score}</h2>
-                    {(!isNameValid) ? 
-                        <div>
+                    {!isNameValid && score > 0 ? 
+                    <>
                         <label>
                             Input your username:
                             <input type="text" name="username" onChange={handleNameChange} />
                         </label>
                     
-                        <button onClick={submitHandling}>
+                        <button onClick={() => submitHandling()}>
                             Submit
                         </button>
-                    </div>
-                    :
-                    <label>
-                        TO HIGHSCORE BUTTON ... THINGS YET TO DO!
-                    </label>
-                    }
+                    </>
+                    : <button onClick={() => alert("Kesken...")}>Show High Scores</button>}
                     
             
                     <button onClick={() => startGame()}>Restart Game</button>
