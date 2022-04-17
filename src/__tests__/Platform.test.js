7227fb24bd826613d2500e0c24143e36f73c53df
unknown <tukako@utu.fi>
2022-04-17 12:43:47 +0300

    Platform testit kommentoitu

--- src/__tests__/Platform.test.js
+++ src/__tests__/Platform.test.js
@@ -3,6 +3,7 @@ import Platform from "../game/Level/Platform";
 describe("Tests about Platform's existence", () => {
     let platform;
     let canvas;
+    // jokaiselle Platformin testille tarvitsee canvasin ja Platformin alustuksen
     beforeEach(() => {
         canvas = document.createElement("canvas");
         canvas.width = 700;
@@ -14,16 +15,19 @@ describe("Tests about Platform's existence", () => {
         expect(platform).toBeTruthy();
     });
 
+    // testataan, että piirtämiseen käytetyt arvot ovat odotetun laiset
     test("Does Platform compute its size correctly", () => {
         expect(platform.getY()).toBe(500-100); // canvas.height - platform's height from bottom
         expect(platform.getWidth()).toBe(80);
         expect(platform.getX()).toBe(0); //left most
     });
 
+    //... ei aiheuta virhettä, näkee suorana ruudulla lopputuloksen
     test("Does draw work?", () => {
         expect(platform.draw());
     });
 
+    // Platformin sijainti päivittyy halutusti moveInX() kutsumalla
     test("Platform is moved correctly",() => {
         const beforeMoving = platform.getX();
         platform.moveInX(5); //moves platform to left
