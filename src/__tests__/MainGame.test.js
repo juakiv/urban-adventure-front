16bc376eb9a7b374dbae51393b8f1ea2d8a12d74
unknown <tukako@utu.fi>
2022-04-19 19:55:12 +0300

    testit vaiheessa, aika loppui

--- src/__tests__/MainGame.test.js
+++ src/__tests__/MainGame.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import MainGame from '../MainGame';
-import { shallow } from 'enzyme';
+import WS from "jest-websocket-mock";
 
 describe("it renders correct content", () => {
+    let server;
+    beforeAll(() => {
+        // create a WS instance, listening on port 1234 on localhost
+        server = new WS("ws://localhost:3001");
+        //await server.connected; // wait for the server to have established the connection
+    });
     
     test("it renders", () => {
         render(<MainGame/>);
@@ -16,8 +22,17 @@ describe("it renders correct content", () => {
         expect(ui).toBeInTheDocument();
     });
 
-    test("it renders correct part when game is running", () => {
-        const mainGameWrapper = shallow( <MainGame/>);
-        console.log(mainGameWrapper.debug());
+    test("it renders correct part when game is running", async () => {
+    
+        render(<MainGame/>);
+        
+
+        const connecting = screen.getByText(/Connecting/);
+        expect(connecting).toBeInTheDocument();
+
+        await server.connected;
+        const gameStart = screen.getByTestId("game-start");        
+        expect(gameStart).toBeInTheDocument();
+        
     });
 });
